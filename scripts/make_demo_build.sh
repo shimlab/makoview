444a411428e177ad042e2585c8744bff629332f0
Oliver Cheng <cheng.o@wehi.edu.au>
2026-05-10 19:59:39 +1000

    Fix demo build

--- scripts/make_demo_build.sh
+++ scripts/make_demo_build.sh
@@ -7,12 +7,20 @@ npm run build -- --base /makoview/
 # replace templates
 python -c "
 import sys, json
-data = open('sample_data.json').read()
+data = open('sample/site.json').read()
 html = sys.stdin.read()
 print(html.replace('__VITE_DATA__', data).replace('window.__DEMO__ = false;', 'window.__DEMO__ = true;'))
-" < dist/_template.html > temp_output.html
+" < dist/_site_template.html > dist/site.html
 
-rm dist/_template.html
-mv temp_output.html dist/demo.html
+
+python -c "
+import sys, json
+data = open('sample/gene.json').read()
+html = sys.stdin.read()
+print(html.replace('__VITE_DATA__', data).replace('window.__DEMO__ = false;', 'window.__DEMO__ = true;'))
+" < dist/_gene_template.html > dist/gene.html
+
+rm dist/_site_template.html
+rm dist/_gene_template.html
 
 mv dist demo_dist
