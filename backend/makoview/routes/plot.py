082737f7eb1419d6399b58d9ec9839af31de6566
Oliver Cheng <cheng.o@wehi.edu.au>
2026-05-10 19:55:09 +1000

    Plot rendering cache

--- backend/makoview/routes/plot.py
+++ backend/makoview/routes/plot.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from fastapi import APIRouter, HTTPException, Request
 from fastapi.responses import Response
+from functools import lru_cache
 
 from ..utils.plots import plot_violin_by_site
 from ..utils.gff import GeneDatabase
@@ -8,23 +9,25 @@ from ..utils.gff import GeneDatabase
 router = APIRouter()
 
 
-@router.get("/binarisedProbabilities")
-async def plotBinarisedProbabilities(id: str, position: int, request: Request):
-    gtf_db: GeneDatabase = request.app.state.gtf_db
+@lru_cache(maxsize=128)
+def cache_plot_wrapper(gtf_db: GeneDatabase, id: str, position: int, binarised: bool):
     rows = gtf_db.get_sample_site_data(id, position)
     if not rows:
         raise HTTPException(status_code=404, detail="No data found for this site")
     df = pd.DataFrame(rows)
-    svg = plot_violin_by_site(df, binarised=True)
+    svg = plot_violin_by_site(df, binarised=binarised)
+    return svg
+
+
+@router.get("/binarisedProbabilities")
+async def plotBinarisedProbabilities(id: str, position: int, request: Request):
+    gtf_db: GeneDatabase = request.app.state.gtf_db
+    svg = cache_plot_wrapper(gtf_db, id, position, binarised=True)
     return Response(content=svg, media_type="image/svg+xml")
 
 
 @router.get("/probabilities")
 async def plotProbabilities(id: str, position: int, request: Request):
     gtf_db: GeneDatabase = request.app.state.gtf_db
-    rows = gtf_db.get_sample_site_data(id, position)
-    if not rows:
-        raise HTTPException(status_code=404, detail="No data found for this site")
-    df = pd.DataFrame(rows)
-    svg = plot_violin_by_site(df, binarised=False)
+    svg = cache_plot_wrapper(gtf_db, id, position, binarised=False)
     return Response(content=svg, media_type="image/svg+xml")
